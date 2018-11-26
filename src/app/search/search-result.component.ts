3debc2362ba806513f4327f943267bcc5184c3dd
Jeroen Ketelaar <jeroen@ketelaar.me>
2018-11-25 18:58:27 -0600

    [BUGFIX] Search results add to cart
    Fixes issue #43

--- src/app/search/search-result.component.ts
+++ src/app/search/search-result.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ProductView} from '@app/models/ProductView';
 import {ProductsService} from '@app/product/products.service';
 import {ActivatedRoute} from '@angular/router';
+import {CartService} from '@app/cart/cart.service';
+import {HeaderComponent} from '@app/shell/header/header.component';
 
 @Component({
     selector: 'app-search',
@@ -12,7 +14,11 @@ export class SearchResultComponent implements OnInit {
     page = 1;
     searchQuery: ProductView[];
 
-    constructor(private productsService: ProductsService, private route: ActivatedRoute) {
+    constructor(
+        private productsService: ProductsService,
+        private route: ActivatedRoute,
+        private cartService: CartService,
+        private header: HeaderComponent) {
     }
 
     ngOnInit() {
@@ -25,5 +31,11 @@ export class SearchResultComponent implements OnInit {
 
     getImageURL(product: ProductView): string {
         return this.productsService.getImageURL(product.singleImage);
+    }
+
+    addToCart(id: number) {
+        this.cartService.add({id: id, quantity: null}).pipe().subscribe(() => {
+            this.header.refreshCart(true);
+        });
     }
 }
