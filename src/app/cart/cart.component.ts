e2c98cc94f872c14ee3fceff2013e571316facf7
Josh Chianelli <joshchianelli@gmail.com>
2018-11-26 12:20:47 -0600

    [CLEANUP] cart comments

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -32,6 +32,9 @@ export class CartComponent implements OnInit {
         this.loadCart();
     }
 
+    /**
+     * Method to retrieve the current cart.
+     */
     loadCart() {
         this.cartService.getCart()
             .pipe()
@@ -40,6 +43,9 @@ export class CartComponent implements OnInit {
             });
     }
 
+    /**
+     * Method to clear the current cart. This removes all items from cart
+     */
     clear() {
         this.cartService.clear()
             .pipe().subscribe(() => {
@@ -47,6 +53,9 @@ export class CartComponent implements OnInit {
         });
     }
 
+    /**
+     * Method to apply a promotion to the cart.
+     */
     apply() {
         this.promotionService.apply({code: this.promoCode}).pipe().subscribe(result => {
             result === 'Error, could not apply promotion' ? this.isValid = false : this.isValid = true;
@@ -54,6 +63,11 @@ export class CartComponent implements OnInit {
         });
     }
 
+    /**
+     * Method to add a product to the cart.
+     *
+     * @param id - The id of the product to be added to the cart.
+     */
     add(id: number) {
         this.cartService.add({id: id, quantity: null}).pipe().subscribe(() => {
             this.loadCart();
@@ -61,6 +75,11 @@ export class CartComponent implements OnInit {
         });
     }
 
+    /**
+     * Method to delete an item from the cart.
+     *
+     * @param id - The id of the product to be deleted from the cart.
+     */
     delete(id: number) {
         this.cartService.delete({id: id, quantity: null}).pipe().subscribe(() => {
             this.loadCart();
@@ -68,6 +87,12 @@ export class CartComponent implements OnInit {
         });
     }
 
+    /**
+     * Method to adjust the quantity of the item in the cart.
+     *
+     * @param id - The id of the product.
+     * @param quantity - The new quantity of the product in the cart.
+     */
     adjust(id: number, quantity: number) {
         this.cartService.adjust({id: id, quantity: quantity}).pipe().subscribe(() => {
             this.loadCart();
@@ -75,30 +100,55 @@ export class CartComponent implements OnInit {
         });
     }
 
+    /**
+     * Returns the total cost before any promotions or discounts.
+     */
     getTotalCost(): number {
         return this.cartService.getTotalCost(this.cart);
     }
 
+    /**
+     * Returns the total cost of the cart after promotions and discounts are applied.
+     */
     getOrderTotal(): number {
         return this.cartService.getOrderTotal(this.cart);
     }
 
+    /**
+     * Returns the total quantity of items in the cart.
+     */
     getTotal(): number {
         return this.cartService.getTotal(this.cart);
     }
 
+    /**
+     * Returns the total amount saved from discounts.
+     */
     getTotalSavings(): number {
         return this.cartService.getTotalSavings(this.cart);
     }
 
+    /**
+     * Returns the total for a specific product including quantities but excluding promotions.
+     *
+     * @param product - An OrderItem object.
+     */
     getTotalCostForProduct(product: OrderItem) {
         return this.cartService.getTotalCostForProduct(product);
     }
 
+    /**
+     * Returns the image url for a specific product.
+     *
+     * @param product - A ProductView object which contains a single image.
+     */
     getImageURL(product: ProductView): string {
         return this.productsService.getImageURL(product.singleImage);
     }
 
+    /**
+     * Returns the promo percentage of the cart.
+     */
     getPromoPercentage(): number {
         return this.cartService.getPromoPercentage(this.cart);
     }
